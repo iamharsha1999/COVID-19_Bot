98a69dc23aeb86c2e415b493840d5ed091bea1e7
Harsha <ashharsha1999@gmail.com>
2020-04-05 11:09:38 +0530

    Updated with world stats

--- map_generator.py
+++ map_generator.py
@@ -3,12 +3,12 @@ import io
 from PIL import Image, ImageDraw, ImageFont
 import json
 import urllib3
-
+import os
 
 if __name__ == "__main__":
 
     print('Fetching Data...')
-
+    print('India Data')
     http = urllib3.PoolManager()
     r = http.request('GET', 'https://api.covid19india.org/data.json')
     data =  json.loads(r.data.decode('utf-8'))
@@ -42,6 +42,16 @@ if __name__ == "__main__":
     active = int(total - (recovered + death))
 
     summ = [total, active, recovered, death]
+    ## World Data
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'https://coronavirus-19-api.herokuapp.com/all')
+    data =  json.loads(r.data.decode('utf-8'))
+    w_t = data['cases']
+    w_d = data['deaths']
+    w_r = data['recovered']
+    w_a = w_t - (w_r + w_d)
+
+    w = [w_t,w_a,w_r,w_d]
 
 
 
@@ -61,20 +71,30 @@ if __name__ == "__main__":
         cx.append(json.loads(points_file.region_shape_attributes[i])['cx'])
         cy.append(json.loads(points_file.region_shape_attributes[i])['cy'])
 
-    back = Image.open('NEW.png')
+    os.replace("./NEW 2.png", "./Posts/NEW 2.png")
+    back = Image.open('./Posts/NEW 2.png')
     draw = ImageDraw.Draw(back)
-    font = ImageFont.truetype("Fonts/Rajdhani-Bold.ttf", 25)
+    font = ImageFont.truetype("Fonts/Rajdhani-Bold.ttf", 27)
     for i,st in enumerate(states[:-4]):
         count = new.loc[new['loc'] == st]['confirmedCases'].values
         count = int(count[0])
         if count !=0:
-            draw.text((cx[i], cy[i]), str(count), font=font, fill = 'red')
+            draw.text((cx[i], cy[i]), str(count), font=font, fill = 'blue')
+
+    font = ImageFont.truetype("Fonts/Rajdhani-Bold.ttf", 30)
 
     j = 0
     for i in range(37,41):
-        draw.text((cx[i], cy[i]-4), str(summ[j]), font=font, fill = 'red')
+        # National Stat
+        draw.text((cx[i]+5, cy[i]), str(summ[j]), font=font, fill = 'red')
+        # World Stat
+        draw.text((cx[i]+115, cy[i]), str(w[j]), font=font, fill = 'red')
         j+=1
 
+    lang = ['english', 'tamil', 'telugu', 'bengali', 'malayalam']
+    for lan in lang:
+        back = back.convert("RGB")
+        back.save('./Posts/'+lan+'.jpg')
+    os.replace("./Posts/NEW 2.png", "./NEW 2.png")
 
-    back.save('english.png')
     print('Image Generated')
